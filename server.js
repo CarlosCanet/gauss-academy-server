cf98e384998730a90a78cf7eda850fdc2ee74953
Carlos Canet <carlos@carlosca.net>
2025-09-10 17:38:43 +0200

    init

---
+++ server.js
@@ -0,0 +1,34 @@
+// ℹ️ Loads environment variables from a .env file into process.env
+try {
+  process.loadEnvFile()
+} catch(error) {
+  console.warn(".env file not found, using default environment values")
+}
+
+import express from "express";
+import config from "./config/index.js";
+import indexRouter from "./routes/index.routes.js";
+import handleErrors from "./errors/index.js";
+import initMongoose from "./db/index.js";
+
+// ℹ️ Establishes a connection to the database
+initMongoose();
+
+// Imports Express (a Node.js framework for handling HTTP requests) and initializes the server
+const app = express();
+// ℹ️ Defines the server port (default: 5005)
+const PORT = process.env.PORT || 5005;
+
+// ℹ️ Loads and applies global middleware (CORS, JSON parsing, etc.) for server configurations
+config(app);
+
+// 👇 Defines and applies route handlers
+app.use("/api", indexRouter);
+
+// ❗ Centralized error handling (must be placed after routes)
+handleErrors(app);
+
+// 🆗 Starts the server
+app.listen(PORT, () => {
+  console.log(`Server listening. Local access on http://localhost:${PORT}`);
+});
